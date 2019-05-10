f93e092611c3abc68caaf287d5af1ee000c387cb
AleioD <alejandrodaquila@hotmail.com>
2019-05-09 21:41:18 -0300

    subiendo los ejercicios hechos en clase

---
+++ Ejercicio2.php
@@ -0,0 +1,10 @@
+<?php
+  $num1 = 25;
+  $num2 = 30;
+
+  if ($num1 > $num2 ) {
+    echo "El número mayor es ​ $num1";
+  }else{
+    echo "El número mayor es ​ $num2";
+  }
+ ?>
---
+++ Ejercicio3.php
@@ -0,0 +1,11 @@
+<?php
+  $num = rand(1, 5);
+
+  echo $num;
+
+  echo "<br>";
+
+  if ($num == 3 || $num == 5) {
+    echo "El número es $num";
+  }
+ ?>
